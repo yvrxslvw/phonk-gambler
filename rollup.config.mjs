9db4a56f19a0afba1068514c4f009cea0ca6ce42
yvrxslvw <yvrxslvv@gmail.com>
2023-10-10 14:03:37 +0200

    Rebuild rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,19 +5,36 @@ import builtinModules from 'builtin-modules';
 
 const pkgJson = jetpack.read('package.json', 'json');
 const packages = [...Object.keys(pkgJson.dependencies)];
+const jsonFile = {
+	name: pkgJson.name,
+	version: pkgJson.version,
+	description: pkgJson.description,
+	scripts: {
+		start: 'node bundle',
+	},
+	author: pkgJson.author,
+	license: pkgJson.license,
+	private: pkgJson.private,
+	dependencies: pkgJson.dependencies,
+};
 
-if (jetpack.exists('./dist/package.json')) jetpack.remove('./dist/package.json');
-jetpack.copy('./package-dist.json', './dist/package.json');
+const distPackagePath = './dist/package.json';
+const distEnvPath = './dist/.env';
 
-if (jetpack.exists('./dist/.env')) jetpack.remove('./dist/.env');
-jetpack.copy('./.env', './dist/.env');
+const input = './src/index.ts';
+const output = {
+	file: './dist/bundle.js',
+	format: 'cjs',
+};
+
+if (jetpack.exists('./dist')) jetpack.remove('./dist');
+
+jetpack.write(distPackagePath, jsonFile);
+jetpack.copy('./.env', distEnvPath);
 
 export default {
-	input: './src/index.ts',
-	output: {
-		file: './dist/bundle.js',
-		format: 'cjs',
-	},
+	input,
+	output,
 	plugins: [typescript(), swc({ minify: true })],
 	external: [...builtinModules, ...packages],
 };
